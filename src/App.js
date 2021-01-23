78e2d560d5b05777b819405fa42f8b22380497db
Piyush55dude <piyushagarwalvo@gmail.com>
2021-01-23 19:25:12 +0530

    load more fuctionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import NavInshorts from './components/NavInshorts';
-import { useEffect, useState  } from "react";
-import NewsContent from './components/NewsContent/NewsContent';
-import Footer from './components/Footer/Footer';
-import axios from 'axios';
+import "./App.css";
+import NavInshorts from "./components/NavInshorts";
+import { useEffect, useState } from "react";
+import NewsContent from "./components/NewsContent/NewsContent";
+import Footer from "./components/Footer/Footer";
+import axios from "axios";
 // import apikey from './data/config';
 
 function App() {
-
   const [category, setCategory] = useState("general");
   const [newsArray, setNewsArray] = useState([]);
   const [newsResults, setNewsResults] = useState();
-  const [loadMore, setLoadMore] = useState(20);
+  const [loadMore, setLoadMore] = useState(15);
 
-
-  const newsApi = async() => {
-
+  const newsApi = async () => {
     try {
-
-      const news = await axios.get(`https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`);
+      const news = await axios.get(
+        `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`
+      );
       // const proxyUrl="https://cors-anywhere.herokuapp.com/";
       // const news = await axios.get(`${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadMore}`);
       setNewsArray(news.data.articles);
@@ -35,9 +33,14 @@ function App() {
 
   return (
     <div className="App">
-    <NavInshorts setcategory={setCategory}/>
-    <NewsContent setLoadMore={setLoadMore} loadMore={loadMore} newsArray={newsArray} newsResults={newsResults}/>
-    <Footer/>
+      <NavInshorts setCategory={setCategory} />
+      <NewsContent
+        setLoadMore={setLoadMore}
+        loadMore={loadMore}
+        newsArray={newsArray}
+        newsResults={newsResults}
+      />
+      <Footer />
     </div>
   );
 }
